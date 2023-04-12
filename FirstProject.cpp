b15e94e690f231f16397bd57857d2a895833b62f
Admin <Admin@DESKTOP-OTMCFOR>
2023-04-12 19:57:52 +0300

    Добавьте файлы проекта.

---
+++ FirstProject.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream> 
+using namespace std; 
+
+int main() {
+    cout << "I LOVE C++!\n";
+    cout << "\n";
+    cout << "\t I LOVE C++!\n";
+    cout << "\n";
+    cout << "\t\t I LOVE C++!\n";
+    cout << "\n";
+    cout << "\t\t\t I LOVE C++!\n";
+    cout << "\n";
+    cout << "\t\t\t\t I LOVE C++!\n";
+}
